7f270abc7e60d43768c2004a86dd04112f409af3
fen1xdown <cameronleecalder@gmail.com>
2019-08-16 11:50:04 -0500

    Working commit

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendsArray = [
+	{
+		name: 'Dr. Disrespect',
+		photo: 'https://pbs.twimg.com/profile_images/1082302298037006337/gWi9Y9Ss_400x400.jpg',
+		scores: ['1','5','5','1','5','5','1','1','1','5']
+	},
+	{
+		name: 'Summit1G',
+		photo: 'https://pbs.twimg.com/profile_images/825045721573314561/efRvTGlf_400x400.jpg',
+		scores: ['2','4','1','1','1','1','1','2','1','5']
+	},
+	{
+		name: 'TimTheTatMan',
+		photo: 'https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2018/12/04195214/TimTheTatman.jpg',
+		scores: ['4','3','5','1','1','5','5','1','1','1']
+	},
+	{
+		name: 'Ninja',
+		photo: 'https://media.comicbook.com/2019/08/ninja-twitch-mixer-1182646.jpeg?auto=webp',
+		scores: ['5','1','2','1','3','4','1','3','3','4']
+	}
+];
+
+module.exports = friendsArray;
